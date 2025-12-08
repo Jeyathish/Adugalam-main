c526b92a5113325e7c8113370c25e93a15f2e1a9
Jeyathish <jeyathish0987@gmail.com>
2025-12-08 18:23:51 +0530

    First commit

---
+++ adugalam_api/settings.py
@@ -0,0 +1,161 @@
+from pathlib import Path
+from datetime import timedelta
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# --------------------------------------------------------
+# SECURITY
+# --------------------------------------------------------
+SECRET_KEY = "django-insecure-55ny(#(km%pc@7eghd4ta!i*fekx^u7k$w0#7a*r$ag5ip#4r3"
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+
+# --------------------------------------------------------
+# INSTALLED APPS
+# --------------------------------------------------------
+INSTALLED_APPS = [
+    # Django default
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # Project apps
+    "users",   
+    "turfs",
+    "bookings",
+    "admin_panel",
+
+    # Third-party
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "corsheaders",
+]
+
+
+AUTH_USER_MODEL = "users.Player"
+
+
+# --------------------------------------------------------
+# MIDDLEWARE
+# --------------------------------------------------------
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",  # must be first
+    "django.middleware.common.CommonMiddleware",
+
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "adugalam_api.urls"
+
+# --------------------------------------------------------
+# TEMPLATES
+# --------------------------------------------------------
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],      # you can add template dirs later
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "adugalam_api.wsgi.application"
+
+# --------------------------------------------------------
+# DATABASE (MYSQL)
+# --------------------------------------------------------
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "adugalam_db",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+# --------------------------------------------------------
+# REST FRAMEWORK
+# --------------------------------------------------------
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+    ],
+}
+
+# --------------------------------------------------------
+# PASSWORD VALIDATION
+# --------------------------------------------------------
+AUTH_PASSWORD_VALIDATORS = [
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+]
+
+# --------------------------------------------------------
+# STATIC & MEDIA
+# --------------------------------------------------------
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+# --------------------------------------------------------
+# SIMPLE JWT
+# --------------------------------------------------------
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
+
+# --------------------------------------------------------
+# CORS
+# --------------------------------------------------------
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ["*"]
+CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+
+# --------------------------------------------------------
+# DEFAULT AUTO FIELD
+# --------------------------------------------------------
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# --------------------------------------------------------
+# LOGGING (DEV)
+# --------------------------------------------------------
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "root": {"handlers": ["console"], "level": "INFO"},
+}
